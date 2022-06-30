bed9063bf20c92675a613cc7bebf83092166943e
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-06-30 13:23:52 +0200

    added title, fixed generate btn and spacing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cardsContainer = document.querySelector('.container')
 const snackbar = document.querySelector('.snackbar')
 const navbar = document.querySelector('.navbar')
+const generateColors = document.querySelector('.generate-colors')
 
 const createRandomDivs = (amount) => {
 	for (let i = 0; i < amount; i++) {
@@ -9,33 +10,36 @@ const createRandomDivs = (amount) => {
 		cardsContainer.appendChild(newDiv)
 	}
 }
-createRandomDivs(100)
+createRandomDivs(30)
 
 const setCardBackground = () => {
 	const cards = document.querySelectorAll('.card')
-    cards.forEach(card => {
-        randomColor = Math.floor(Math.random() * 16777215).toString(16)
-        card.style.backgroundColor = `#${randomColor}`
-        card.innerHTML = `<span data-color='${randomColor}'>#${randomColor}</span>`
+	cards.forEach((card) => {
+		randomColor = Math.floor(Math.random() * 16777215).toString(16)
+		card.style.backgroundColor = `#${randomColor}`
+		card.innerHTML = `<span data-color='${randomColor}'>#${randomColor}</span>`
 
-        card.addEventListener('click', (e) => {
-            let currentColor = e.target.querySelector('span').getAttribute('data-color')
-            navigator.clipboard.writeText(`#${currentColor}`)
-            snackbar.classList.add('active')
-            setTimeout(() =>{
-                snackbar.classList.remove('active')
-            }, 1500)
-        })
-    })
+		card.addEventListener('click', (e) => {
+			let currentColor = e.target.querySelector('span').getAttribute('data-color')
+			navigator.clipboard.writeText(`#${currentColor}`)
+			snackbar.classList.add('active')
+			document.execCommand('copy')
+			setTimeout(() => {
+				snackbar.classList.remove('active')
+			}, 1500)
+		})
+	})
 }
-
-genNew.addEventListener('click', setCardBackground)
-setCardBackground();
+setCardBackground()
 
 document.addEventListener('scroll', () => {
-    if(scrollY > 100){
-        navbar.classList.add('active')
-    } else{
-        navbar.classList.remove('active')
-    }
+	if (scrollY > 100) {
+		navbar.classList.add('active')
+	} else {
+		navbar.classList.remove('active')
+	}
+})
+
+generateColors.addEventListener('click', () => {
+	setCardBackground()
 })
